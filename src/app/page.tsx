58073a131d3a7da2c9033d051972ddcd20400aab
OscarJRM <oramirez0004@uta.edu.ec>
2024-12-19 03:39:16 -0500

    intefaz empleado registrar alquiler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,64 @@ import Cookies from "js-cookie";
 
 export default function Home() {
   const router = useRouter();
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   useEffect(() => {
-    const token = Cookies.get("auth"); // Obtiene el token desde las cookies
-    const userDataString = localStorage.getItem("userData");
-    const userData = userDataString ? JSON.parse(userDataString) : null; // Obtiene los datos del usuario desde localStorage
+    const validateAuth = async () => {
+      const token = Cookies.get("auth"); // Obtiene el token de las cookies
 
-    if (!token || !userData) {
-      // Si no hay token o datos del usuario, redirige al login
-      router.push("/pages/auth/Login");
-      return;
-    }
-
-    // Verifica el rol del usuario y redirige según corresponda
-    switch (userData.rol) {
-      case "admin":
-        router.push("/pages/admin");
-        break;
-      case "empleado":
-        router.push("/pages/employee");
-        break;
-      case "cliente":
-        router.push("/pages/customer");
-        break;
-      default:
-        // Si el rol no es reconocido, redirige al login
+      if (!token) {
+        // Si no hay token, redirige al login
         router.push("/pages/auth/Login");
-    }
+        return;
+      }
+
+      try {
+        // Verifica el token con el backend
+// Redirige al usuario según su rol después de verificar el token
+        const userDataString = localStorage.getItem("userData");
+        if (!userDataString) {
+          throw new Error("No user data found in localStorage");
+        }
+        const userData = JSON.parse(userDataString);
+
+        if (!userData) {
+          throw new Error("No user data found in localStorage");
+        }
+        const response = await fetch(`${API_URL}/users/${userData.cedula}`, {
+          method: "GET",
+          credentials: "include", // Incluye el token automáticamente
+        });
+
+        if (!response.ok) {
+          throw new Error("Token inválido o expirado");
+        }
+
+        
+
+        switch (userData.rol) {
+          case "admin":
+            router.push("/pages/admin");
+            break;
+          case "empleado":
+            router.push("/pages/employee");
+            break;
+          case "cliente":
+            router.push("/pages/customer");
+            break;
+          default:
+            router.push("/pages/auth/Login");
+        }
+      } catch (error) {
+
+        // Si el token es inválido, elimina la cookie y redirige al login
+        Cookies.remove("auth");
+        localStorage.removeItem("userData");
+        router.push("/pages/auth/Login");
+      }
+    };
+
+    validateAuth();
   }, [router]);
 
   return null; // No renderiza nada mientras redirige
