c872893cbb31004b2cebee0726c5fddc44a0b81c
Karen-Mich <kguatumillo3914@uta.edu.ec>
2024-12-19 11:57:04 -0500

    Agregar campos obligatorios

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -23,6 +23,7 @@ export default function InputField({
           onChange={onChange}
           className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-gray-50 text-gray-900 rounded-md shadow-sm focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
           placeholder={placeholder}
+          required
         />
       </div>
     );
