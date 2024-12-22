5e24aa055ca4bdc9d08882f30a6ac399c7190a3f
OscarJRM <oramirez0004@uta.edu.ec>
2024-12-22 01:33:28 -0500

    label precio maximo customer

--- src/app/pages/customer/page.tsx
+++ src/app/pages/customer/page.tsx
@@ -57,7 +57,10 @@ const Customer: React.FC = () => {
     return days > 0 ? days * (selectedVehicle?.tarifas[0]?.tarifa || 0) : 0;
   };
 
-  const handleReservation = async (event: React.FormEvent, vehicle: Vehicle) => {
+  const handleReservation = async (
+    event: React.FormEvent,
+    vehicle: Vehicle
+  ) => {
     event.preventDefault();
     const userDataString = localStorage.getItem("userData");
     const userData = userDataString ? JSON.parse(userDataString) : null;
@@ -108,7 +111,10 @@ const Customer: React.FC = () => {
       .then((response) => response.json())
       .then((data) => {
         const filteredData = data
-          .filter((vehicle: Vehicle) => vehicle.estado !== "Eliminado" && vehicle.estado == "Disponible")
+          .filter(
+            (vehicle: Vehicle) =>
+              vehicle.estado !== "Eliminado" && vehicle.estado == "Disponible"
+          )
 
           .map((vehicle: Vehicle) => ({
             ...vehicle,
@@ -126,8 +132,12 @@ const Customer: React.FC = () => {
       });
   }, []);
 
-  const uniqueBrands = Array.from(new Set(vehicles.map((vehicle) => vehicle.marca)));
-  const uniqueYears = Array.from(new Set(vehicles.map((vehicle) => vehicle.anio)));
+  const uniqueBrands = Array.from(
+    new Set(vehicles.map((vehicle) => vehicle.marca))
+  );
+  const uniqueYears = Array.from(
+    new Set(vehicles.map((vehicle) => vehicle.anio))
+  );
 
   useEffect(() => {
     let filtered = vehicles;
@@ -137,7 +147,9 @@ const Customer: React.FC = () => {
     }
 
     if (filterPrice > 0) {
-      filtered = filtered.filter((vehicle) => vehicle.tarifas[0]?.tarifa <= filterPrice);
+      filtered = filtered.filter(
+        (vehicle) => vehicle.tarifas[0]?.tarifa <= filterPrice
+      );
     }
 
     if (filterAvailability === "Disponible") {
@@ -172,15 +184,24 @@ const Customer: React.FC = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Catálogo de Vehículos</h1>
-      <p className="text-center text-gray-700 mb-8">Explora los vehículos disponibles para alquilar</p>
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+        Catálogo de Vehículos
+      </h1>
+      <p className="text-center text-gray-700 mb-8">
+        Explora los vehículos disponibles para alquilar
+      </p>
 
       {/* Filtros */}
       <div className="filters flex flex-wrap justify-center gap-4 mb-8">
         <label htmlFor="filterBrand" className="sr-only">
           Marca
         </label>
-        <select id="filterBrand" value={filterBrand} onChange={(e) => setFilterBrand(e.target.value)} className="p-2 border rounded-lg text-gray-800 bg-white">
+        <select
+          id="filterBrand"
+          value={filterBrand}
+          onChange={(e) => setFilterBrand(e.target.value)}
+          className="p-2 border rounded-lg text-gray-800 bg-white"
+        >
           <option value="Todas">Todas las marcas</option>
           {uniqueBrands.map((brand) => (
             <option key={brand} value={brand}>
@@ -188,23 +209,31 @@ const Customer: React.FC = () => {
             </option>
           ))}
         </select>
-
-        <input id="price-input" type="text" placeholder="Precio Máximo (ej. 20000)" value={filterPrice} onChange={(e) => setFilterPrice(Number(e.target.value.replace(/\D/g, "")))} className="p-2 border rounded-lg text-gray-800 bg-white" />
-
-        {/*
+        <div className="flex items-center space-x-2">
+          <label htmlFor="price-input" className="text-gray-700 font-medium">
+            Precio Máximo
+          </label>
+          <input
+            id="price-input"
+            type="text"
+            placeholder="Precio Máximo (ej. 20000)"
+            value={filterPrice}
+            onChange={(e) =>
+              setFilterPrice(Number(e.target.value.replace(/\D/g, "")))
+            }
+            className="p-2 border rounded-lg text-gray-800 bg-white"
+          />
+        </div>
         <select
-          id="status-cmbx"
-          value={filterAvailability}
-          onChange={(e) => setFilterAvailability(e.target.value)}
+          id="year-cmbx"
+          value={filterYear}
+          onChange={(e) =>
+            setFilterYear(
+              e.target.value === "Todos" ? "Todos" : Number(e.target.value)
+            )
+          }
           className="p-2 border rounded-lg text-gray-800 bg-white"
         >
-          <option value="Todos">Todos los estados</option>
-          <option value="Disponible">Disponible</option>
-          <option value="Alquilado">Alquilado</option>
-        </select>
-*/}
-
-        <select id="year-cmbx" value={filterYear} onChange={(e) => setFilterYear(e.target.value === "Todos" ? "Todos" : Number(e.target.value))} className="p-2 border rounded-lg text-gray-800 bg-white">
           <option value="Todos">Todos los años</option>
           {uniqueYears.map((year) => (
             <option key={year} value={year}>
@@ -213,7 +242,11 @@ const Customer: React.FC = () => {
           ))}
         </select>
 
-        <button id="clear-btn" onClick={resetFilters} className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition duration-200">
+        <button
+          id="clear-btn"
+          onClick={resetFilters}
+          className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition duration-200"
+        >
           Borrar Filtros
         </button>
       </div>
@@ -225,30 +258,48 @@ const Customer: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {filteredVehicles.length > 0 ? (
             filteredVehicles.map((vehicle) => (
-              <div key={vehicle._id} className="bg-white p-6 rounded-xl shadow-lg border hover:shadow-2xl transition-shadow duration-300">
+              <div
+                key={vehicle._id}
+                className="bg-white p-6 rounded-xl shadow-lg border hover:shadow-2xl transition-shadow duration-300"
+              >
                 {/* Imagen del vehículo */}
                 <div className="relative h-40 w-full overflow-hidden rounded-xl mb-4">
-                  <img src={vehicle.imagen} alt={vehicle.nombre} className="object-cover w-full h-full transform hover:scale-105 transition-transform duration-300" />
+                  <img
+                    src={vehicle.imagen}
+                    alt={vehicle.nombre}
+                    className="object-cover w-full h-full transform hover:scale-105 transition-transform duration-300"
+                  />
                 </div>
 
                 {/* Información del vehículo */}
-                <h2 className="text-xl font-semibold text-gray-800">{vehicle.nombre}</h2>
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {vehicle.nombre}
+                </h2>
                 <p className="text-sm text-gray-500">{vehicle.marca}</p>
                 <p className="text-gray-600 my-2">
                   <strong>Modelo:</strong> {vehicle.modelo}
                 </p>
                 <p className="text-gray-600">
-                  <strong>Precio por día:</strong> <span className="text-green-600 font-semibold">${vehicle.tarifas[0]?.tarifa || 0}</span>
+                  <strong>Precio por día:</strong>{" "}
+                  <span className="text-green-600 font-semibold">
+                    ${vehicle.tarifas[0]?.tarifa || 0}
+                  </span>
                 </p>
 
                 {/* Botón de acción */}
-                <button id="reservar-btn" onClick={() => openModal(vehicle)} className="mt-4 w-full bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200">
+                <button
+                  id="reservar-btn"
+                  onClick={() => openModal(vehicle)}
+                  className="mt-4 w-full bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200"
+                >
                   Reservar Ahora
                 </button>
               </div>
             ))
           ) : (
-            <p className="text-center col-span-full text-red-500 font-bold">No hay vehículos que coincidan con los filtros seleccionados.</p>
+            <p className="text-center col-span-full text-red-500 font-bold">
+              No hay vehículos que coincidan con los filtros seleccionados.
+            </p>
           )}
         </div>
       )}
@@ -258,14 +309,23 @@ const Customer: React.FC = () => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg relative">
             {/* Botón para cerrar el modal */}
-            <button onClick={closeModal} className="absolute top-2 right-6 text-2xl font-bold text-gray-600 hover:text-red-600">
+            <button
+              onClick={closeModal}
+              className="absolute top-2 right-6 text-2xl font-bold text-gray-600 hover:text-red-600"
+            >
               &times;
             </button>
 
             {/* Detalles del vehículo */}
-            <h2 className="text-2xl font-bold text-center mb-4 text-gray-800">{selectedVehicle.nombre}</h2>
+            <h2 className="text-2xl font-bold text-center mb-4 text-gray-800">
+              {selectedVehicle.nombre}
+            </h2>
             <div className="flex justify-center mb-4">
-              <img src={selectedVehicle.imagen} alt={selectedVehicle.nombre} className="w-96 h-56 object-cover rounded-lg" />
+              <img
+                src={selectedVehicle.imagen}
+                alt={selectedVehicle.nombre}
+                className="w-96 h-56 object-cover rounded-lg"
+              />
             </div>
 
             <div className="text-left mb-4">
@@ -283,21 +343,30 @@ const Customer: React.FC = () => {
                 <strong>Color:</strong> {selectedVehicle.color}
               </p>
               <p className="text-gray-700">
-                <strong>Precio por día:</strong> ${selectedVehicle.tarifas[0]?.tarifa || 0}
+                <strong>Precio por día:</strong> $
+                {selectedVehicle.tarifas[0]?.tarifa || 0}
               </p>
             </div>
 
             {/* Botón para desplegar la sección de fechas */}
-            <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition duration-200">
+            <button
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition duration-200"
+            >
               {isSidebarOpen ? "Ocultar Fechas" : "Seleccionar Fechas"}
             </button>
 
             {/* Sección de selección de fechas */}
             {isSidebarOpen && (
               <div className="mt-4">
-                <h3 className="text-lg font-semibold mb-2 text-gray-800">Selecciona las fechas de tu reserva</h3>
+                <h3 className="text-lg font-semibold mb-2 text-gray-800">
+                  Selecciona las fechas de tu reserva
+                </h3>
                 <div className="mb-4">
-                  <label htmlFor="fechaInicio" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="fechaInicio"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Fecha de inicio
                   </label>
                   <input
@@ -309,7 +378,10 @@ const Customer: React.FC = () => {
                       setFechaInicio(nuevaFechaInicio);
 
                       // Reiniciar fecha de fin si ya no es válida
-                      if (fechaFin && new Date(nuevaFechaInicio) > new Date(fechaFin)) {
+                      if (
+                        fechaFin &&
+                        new Date(nuevaFechaInicio) > new Date(fechaFin)
+                      ) {
                         setFechaFin(""); // Resetea la fecha de fin si la fecha de inicio cambia a una posterior
                       }
                     }}
@@ -320,7 +392,10 @@ const Customer: React.FC = () => {
                 </div>
 
                 <div className="mb-4">
-                  <label htmlFor="fechaFin" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="fechaFin"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Fecha de fin
                   </label>
                   <input
@@ -332,7 +407,9 @@ const Customer: React.FC = () => {
                       if (new Date(nuevaFechaFin) >= new Date(fechaInicio)) {
                         setFechaFin(nuevaFechaFin);
                       } else {
-                        alert("La fecha de fin no puede ser anterior a la fecha de inicio.");
+                        alert(
+                          "La fecha de fin no puede ser anterior a la fecha de inicio."
+                        );
                         setFechaFin(""); // Resetea la fecha de fin si es inválida
                       }
                     }}
@@ -344,11 +421,16 @@ const Customer: React.FC = () => {
                 </div>
 
                 <div className="mb-4">
-                  <p className="text-lg font-bold text-gray-800">Total: ${calcularTotal()} USD</p>
+                  <p className="text-lg font-bold text-gray-800">
+                    Total: ${calcularTotal()} USD
+                  </p>
                 </div>
 
                 {/* Botón para confirmar la reserva */}
-                <button onClick={() => setShowConfirmModal(true)} className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition duration-200">
+                <button
+                  onClick={() => setShowConfirmModal(true)}
+                  className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 transition duration-200"
+                >
                   Confirmar Reserva
                 </button>
               </div>
@@ -358,15 +440,29 @@ const Customer: React.FC = () => {
             {showConfirmModal && (
               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                 <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-                  <h2 className="text-xl font-bold text-center mb-4 text-gray-800">Confirmar Reserva</h2>
+                  <h2 className="text-xl font-bold text-center mb-4 text-gray-800">
+                    Confirmar Reserva
+                  </h2>
                   <p className="text-gray-700 mb-4">
-                    Estás a punto de reservar el vehículo <strong>{selectedVehicle.nombre}</strong> del <strong>{fechaInicio}</strong> al <strong>{fechaFin}</strong>. El total será <strong>${calcularTotal()}</strong>.
+                    Estás a punto de reservar el vehículo{" "}
+                    <strong>{selectedVehicle.nombre}</strong> del{" "}
+                    <strong>{fechaInicio}</strong> al{" "}
+                    <strong>{fechaFin}</strong>. El total será{" "}
+                    <strong>${calcularTotal()}</strong>.
                   </p>
                   <div className="flex justify-between">
-                    <button onClick={() => setShowConfirmModal(false)} className="bg-gray-600 text-white py-2 px-4 rounded-lg hover:bg-gray-700">
+                    <button
+                      onClick={() => setShowConfirmModal(false)}
+                      className="bg-gray-600 text-white py-2 px-4 rounded-lg hover:bg-gray-700"
+                    >
                       Cancelar
                     </button>
-                    <button onClick={(event) => handleReservation(event, selectedVehicle!)} className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700">
+                    <button
+                      onClick={(event) =>
+                        handleReservation(event, selectedVehicle!)
+                      }
+                      className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700"
+                    >
                       Confirmar
                     </button>
                   </div>
